df641fa2eafc508ab040a527933c90b64e40a4d6
Федор Глеб | Abhay Gaur <59841219+snehccurry@users.noreply.github.com>
2022-07-18 20:20:52 +0530

    Update blurwindowtest.py

--- blurwindowtest.py
+++ blurwindowtest.py
@@ -2,8 +2,8 @@
 from tkinter import *
 import ctypes
 from ctypes import windll
-from BlurWindow.blurWindow import blur,Win7Blur,GlobalBlur
-
+from BlurWindow.blurWindow import blur,GlobalBlur
+import inspect
 #root = Tk()
 
 
@@ -12,15 +12,29 @@ def blurrer(window):
     root=window
     mainWindow=window
     HWND = windll.user32.GetParent(mainWindow.winfo_id())
-    root.config(bg='green')
-
-    root.wm_attributes("-transparent", 'green')
-    root.geometry('500x400')
+    #root.config(bg='green')
+    #root.attributes('-alpha',1)
+    print(root.wm_attributes())
+    #root.wm_attributes("-transparent", 'green')
+    #root.geometry('500x400')
     root.update()
     HWND = windll.user32.GetParent(mainWindow.winfo_id())
 
-    GlobalBlur(HWND,Acrylic=True) #Enable Acrylic
-    print("reached")
+    GlobalBlur(HWND,hexColor='#11111199',Acrylic=True, Dark=True) #Enable Acrylic
+    #111111FF #for super dark mode
+    #11111199 #for dark mode
+    #30121244 #for light mode
+    #66999999 #for light blue-cyan theme
+    #44999999 #for light bluish theme
+    #44009999 #for purple theme
+    #30121244 #for reddish purple
+    #30121277 #for more redish purple
+    #99004444 #for purple
+    #99000044 #for reddish
+    #99000044 #for dark redish
+    #99000099 #for full redish
+    print(inspect.getargspec(GlobalBlur))
+   
     
     global ACRYLIC_ENABLED
     ACRYLIC_ENABLED = True
@@ -35,12 +49,12 @@ def blurrer(window):
                 GlobalBlur(HWND,Acrylic=False)
             else:
                 root.after_cancel(DRAG) #cancel task \/ (is dragging)
-            DRAG = root.after(200, stop_drag) #execute stop_drag after 200ms
+            DRAG = root.after(100, stop_drag) #execute stop_drag after 200ms
 
     def stop_drag():
         global DRAG
         DRAG = False
-        GlobalBlur(HWND,Acrylic=True) 
+        GlobalBlur(HWND,hexColor='#11111199',Acrylic=True,Dark=True,) 
 
     root.bind('<Configure>', dragging)
 
