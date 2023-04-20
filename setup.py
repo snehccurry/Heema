e73212e3ce6d76b0e1a4b4faa5dee4d87821ea8b
Федор Глеб | Abhay Gaur <59841219+snehccurry@users.noreply.github.com>
2023-04-21 02:08:56 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = "1.0.0.1"
+DESCRIPTION = 'A GUI framework built on top of tkinter, with new features.'
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="Heema",
+    version=VERSION,
+    author="Федор Глеб | Abhay Gaur",
+    author_email="<abhay.12104531@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['BlurWindow', 'pillow', 'pyaudio'],
+    keywords=['Heema','heema','heema gui','HEEMA','heemagui','python', 'tkinter', 'modern', 'gui', 'tkintergui', 'browser'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
