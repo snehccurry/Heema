fb75508743de55d55c795c54e3714f0e6dfe3365
Федор Глеб | Abhay Gaur <59841219+snehccurry@users.noreply.github.com>
2023-04-23 01:10:49 +0530

    updated example

--- test2.py
+++ test2.py
@@ -3,13 +3,16 @@ from Heema import *
 from Heema import tk 
 from pages import *
 
-root=tk.Tk()
-title_bar=title_bar(root,text="Abhay")
-root.geometry("1200x900")
+window1=tk.Tk()
+title_bar=title_bar(window1,text="Abhay")
+Zen_mode(window1)
 
 
+main_frame=LabelFrame(window1,bd=0,bg=label_bg)
+main_frame.pack(fill=BOTH,expand=True)
 
-menu_bar=menu_bar(root)
+
+menu_bar=menu_bar(main_frame)
 #menu_bar.pack(fill=X,side=TOP)
 menu_button1=menu_button(menu_bar, text="Files",command=files_page)
 menu_button2=menu_button(menu_bar, text="Edit",command=edit_page)
@@ -18,6 +21,19 @@ menu_button4=menu_button(menu_bar, text="Views",command=views_page)
 menu_button5=menu_button(menu_bar, text="Settings",command=settings_page)
 menu_button6=menu_button(menu_bar, text="Run",command=run_page)
 menu_button7=menu_button(menu_bar, text="Help",command=help_page)
+
+
+
+def Welcome_page():
+    Welcome_page=LabelFrame(bd=0,bg=label_bg)
+
+    b1=button(Welcome_page,text="Hi",command=do_nothing)
+    b1.pack()
+
+    label1=label_button(Welcome_page,text="Open Settings",command=do_nothing)
+    label1.pack()
+
+    return Welcome_page
 
 
 
@@ -39,92 +55,99 @@ def printer2():
 	print(f"Button 2 printed succesfully")
 
 
-left_frame=left_frame(frame_name=root)
+left_frame=left_frame(frame_name=main_frame)
 
 
-b=left_frame_button(left_frame,text="Classic",command=lambda: apply_theme(root,classic))
+b=left_frame_button(left_frame,text="Classic",command=lambda: apply_theme(window1,classic))
 b.configure(font=('calibri','12'))
 
 
 
-c=left_frame_button(left_frame,text="Dark",command=lambda: apply_theme(root,dark_mode))
+c=left_frame_button(left_frame,text="Dark",command=lambda: apply_theme(window1,dark_mode))
 c.configure(font=('calibri','12'))
 
 
-d=left_frame_button(left_frame,text="Super Dark",command=lambda: apply_theme(root,super_dark_mode))
+d=left_frame_button(left_frame,text="Super Dark",command=lambda: apply_theme(window1,super_dark_mode))
 d.configure(font=('calibri','12'))
 
 
-e=left_frame_button(left_frame,text="Light",command=lambda: apply_theme(root,light_mode))
+e=left_frame_button(left_frame,text="Light",command=lambda: apply_theme(window1,light_mode))
 e.configure(font=('calibri','12'))
 
 
-f=left_frame_button(left_frame,text="Light Blue",command=lambda: apply_theme(root,light_bluish_mode))
+f=left_frame_button(left_frame,text="Light Blue",command=lambda: apply_theme(window1,light_bluish_mode))
 f.configure(font=('calibri','12'))
 
 
-g=left_frame_button(left_frame,text="Blue Cyan",command=lambda: apply_theme(root,light_blue_cyan_mode))
+g=left_frame_button(left_frame,text="Blue Cyan",command=lambda: apply_theme(window1,light_blue_cyan_mode))
 g.configure(font=('calibri','12'))
 
 
-h=left_frame_button(left_frame,text="Purple Mode",command=lambda: apply_theme(root,purple_mode))
+h=left_frame_button(left_frame,text="Purple Mode",command=lambda: apply_theme(window1,purple_mode))
 h.configure(font=('calibri','12'))
 
 
-i=left_frame_button(left_frame,text="ReddishPurple",command=lambda: apply_theme(root,reddish_purple))
+i=left_frame_button(left_frame,text="ReddishPurple",command=lambda: apply_theme(window1,reddish_purple))
 i.configure(font=('calibri','12'))
 
 
-j=left_frame_button(left_frame,text="ReddishPurple+",command=lambda: apply_theme(root,more_reddish_purple))
+j=left_frame_button(left_frame,text="ReddishPurple+",command=lambda: apply_theme(window1,more_reddish_purple))
 j.configure(font=('calibri','12'))
 
 
-k=left_frame_button(left_frame,text="Purple",command=lambda: apply_theme(root,purple))
+k=left_frame_button(left_frame,text="Purple",command=lambda: apply_theme(window1,purple))
 k.configure(font=('calibri','12'))
 
 
-l=left_frame_button(left_frame,text="Reddish",command=lambda: apply_theme(root,reddish))
+l=left_frame_button(left_frame,text="Reddish",command=lambda: apply_theme(window1,reddish))
 l.configure(font=('calibri','12'))
 
 
-m=left_frame_button(left_frame,text="Reddish+",command=lambda: apply_theme(root,full_reddish))
+m=left_frame_button(left_frame,text="Reddish+",command=lambda: apply_theme(window1,full_reddish))
 m.configure(font=('calibri','12'))
 
 
-#root.title("Hello")
+#main_frame.title("Hello")
 
-#root.geometry("400x400")
-
-
-a=button(frame_name=root,text="Hello world",command=printer)
-a.pack(side=TOP)
+#main_frame.geometry("400x400")
 
 
+a=button2(frame_name=main_frame,text="Hello world",command=printer)
+a.pack(side=TOP, ipadx=50)
 
-a2=button(frame_name=root,text="Works",command=printer2)
-a2.pack(side=TOP)
 
 
-l1=label_button(frame_name=root,text="My Microsoft account")
+
+
+welcome_page=Welcome_page()
+a2=button(frame_name=main_frame,text="Hello world",command=lambda: navigate_to(main_frame ,welcome_page))
+a2.pack(side=TOP, ipadx=50)
+
+
+
+
+l1=label_button(frame_name=main_frame,text="My Microsoft account")
 l1.pack(side=TOP)
 
 
 
 #some serious fun is going on here, XDD funny as fuck lol
-#s1=ttk.Button(root,text="OLD FASHINOED WINDOWS BUTTON LOL").pack()
-#button1=Heema.tk.Button(root,text="Abhay Gaur",="#44444").pack()
+#s1=ttk.Button(main_frame,text="OLD FASHINOED WINDOWS BUTTON LOL").pack()
+#button1=Heema.tk.Button(main_frame,text="Abhay Gaur",="#44444").pack()
 
 
-labelframe1=LabelFrame(root,bg=bg ,text="Buttons for next",bd=label_bd)
+labelframe1=LabelFrame(main_frame,bg=bg ,text="Buttons for next",bd=label_bd)
 labelframe1.pack()
+
+
 """
-
-
 from PIL import ImageTk, Image
 my_img1=ImageTk.PhotoImage(Image.open("Screenshots/Smallview.png"))
 
 image_label=Label(image=my_img1)
 image_label.pack()
+
+"""
 
 
 
@@ -135,12 +158,12 @@ if global_theme=='light_mode':
     image_label.forget()
 
 
-"""
+
 
 
 
 ##################			ROOT MAINLOOP()
-root.mainloop()
+main_frame.mainloop()
 
 
 
@@ -199,15 +222,15 @@ root.mainloop()
 
 
 
-#root.wm_attributes("-transparentcolor", '#F0F0F0')
-#print(root.attributes())
-#print(root.wm_attributes())
+#main_frame.wm_attributes("-transparentcolor", '#F0F0F0')
+#print(main_frame.attributes())
+#print(main_frame.wm_attributes())
 
 
 
 
-#apply_theme(root,super_dark_mode) #applies the windowsxp theme, it's what windows 10 actually uses.
-#apply_theme(root,classic)
+#apply_theme(main_frame,super_dark_mode) #applies the windowsxp theme, it's what windows 10 actually uses.
+apply_theme(window1,classic)
 """
 super_dark_mode='#111111FF' #for super dark mode
 dark_mode='#11111199' #for dark mode
@@ -370,24 +393,24 @@ full_reddish'#99000099' #for full reddish
 
 tk_title = "tk" # Put here your window title
 
-# root (your app doesn't go in root, it goes in window)
-root.title(tk_title) 
-root.overrideredirect(True) # turns off title bar, geometry
-root.geometry('200x200+75+75') # set new geometry the + 75 + 75 is where it starts on the screen
-#root.iconbitmap("your_icon.ico") # to show your own icon 
-root.minimized = False # only to know if root is minimized
-root.maximized = False # only to know if root is maximized
+# main_frame (your app doesn't go in main_frame, it goes in window)
+main_frame.title(tk_title) 
+main_frame.overrideredirect(True) # turns off title bar, geometry
+main_frame.geometry('200x200+75+75') # set new geometry the + 75 + 75 is where it starts on the screen
+#main_frame.iconbitmap("your_icon.ico") # to show your own icon 
+main_frame.minimized = False # only to know if main_frame is minimized
+main_frame.maximized = False # only to know if main_frame is maximized
 
 LGRAY = '#3e4042' # button color effects in the title bar (Hex color)
 DGRAY = '#25292e' # window background color               (Hex color)
 RGRAY = '#10121f' # title bar color                       (Hex color)
 
-root.config(bg="#25292e")
-title_bar = Frame(root, bg=RGRAY, relief='raised', bd=0,highlightthickness=0)
+main_frame.config(bg="#25292e")
+title_bar = Frame(main_frame, bg=RGRAY, relief='raised', bd=0,highlightthickness=0)
 
 
 def set_appwindow(mainWindow): # to display the window icon on the taskbar, 
-                               # even when using root.overrideredirect(True
+                               # even when using main_frame.overrideredirect(True
     # Some WindowsOS styles, required for task bar integration
     GWL_EXSTYLE = -20
     WS_EX_APPWINDOW = 0x00040000
@@ -404,41 +427,41 @@ def set_appwindow(mainWindow): # to display the window icon on the taskbar,
     
 
 def minimize_me():
-    root.attributes("-alpha",0) # so you can't see the window when is minimized
-    root.minimized = True       
+    main_frame.attributes("-alpha",0) # so you can't see the window when is minimized
+    main_frame.minimized = True       
 
 
 def deminimize(event):
 
-    root.focus() 
-    root.attributes("-alpha",1) # so you can see the window when is not minimized
-    if root.minimized == True:
-        root.minimized = False                              
+    main_frame.focus() 
+    main_frame.attributes("-alpha",1) # so you can see the window when is not minimized
+    if main_frame.minimized == True:
+        main_frame.minimized = False                              
         
 
 def maximize_me():
 
-    if root.maximized == False: # if the window was not maximized
-        root.normal_size = root.geometry()
+    if main_frame.maximized == False: # if the window was not maximized
+        main_frame.normal_size = main_frame.geometry()
         expand_button.config(text=" 🗗 ",font=("calibri",14))
-        root.geometry(f"{root.winfo_screenwidth()}x{root.winfo_screenheight()}+0+0")
-        root.maximized = not root.maximized 
+        main_frame.geometry(f"{main_frame.winfo_screenwidth()}x{main_frame.winfo_screenheight()}+0+0")
+        main_frame.maximized = not main_frame.maximized 
         # now it's maximized
         
     else: # if the window was maximized
         expand_button.config(text=" 🗖 ")
-        root.geometry(root.normal_size)
-        root.maximized = not root.maximized
+        main_frame.geometry(main_frame.normal_size)
+        main_frame.maximized = not main_frame.maximized
         # now it is not maximized
 
 # put a close button on the title bar
-close_button = Button(title_bar, text='  ×  ', command=root.destroy,bg=RGRAY,padx=2,pady=2,font=("calibri", 13),bd=0,fg='white',highlightthickness=0)
+close_button = Button(title_bar, text='  ×  ', command=main_frame.destroy,bg=RGRAY,padx=2,pady=2,font=("calibri", 13),bd=0,fg='white',highlightthickness=0)
 expand_button = Button(title_bar, text=' 🗖 ', command=maximize_me,bg=RGRAY,padx=2,pady=2,bd=0,fg='white',font=("calibri", 14),highlightthickness=0)
 minimize_button = Button(title_bar, text=' ─ ',command=minimize_me,bg=RGRAY,padx=2,pady=2,bd=0,fg='white',font=("calibri", 14),highlightthickness=0)
 title_bar_title = Label(title_bar, text=tk_title, bg=RGRAY,bd=0,fg='white',font=("helvetica", 10),highlightthickness=0)
 
 # a frame for the main area of the window, this is where the actual app will go
-window = Frame(root, bg=DGRAY,highlightthickness=0)
+window = Frame(main_frame, bg=DGRAY,highlightthickness=0)
 
 
 def changex_on_hovering(event):
@@ -472,24 +495,24 @@ def returnm_size_on_hovering(event):
     
 
 def get_pos(event): # this is executed when the title bar is clicked to move the window
-    if root.maximized == False:
+    if main_frame.maximized == False:
  
-        xwin = root.winfo_x()
-        ywin = root.winfo_y()
-        startx = event.x_root
-        starty = event.y_root
+        xwin = main_frame.winfo_x()
+        ywin = main_frame.winfo_y()
+        startx = event.x_main_frame
+        starty = event.y_main_frame
 
         ywin = ywin - starty
         xwin = xwin - startx
 
         
         def move_window(event): # runs when window is dragged
-            root.config(cursor="fleur")
-            root.geometry(f'+{event.x_root + xwin}+{event.y_root + ywin}')
+            main_frame.config(cursor="fleur")
+            main_frame.geometry(f'+{event.x_main_frame + xwin}+{event.y_main_frame + ywin}')
 
 
         def release_window(event): # runs when window is released
-            root.config(cursor="arrow")
+            main_frame.config(cursor="arrow")
             
             
         title_bar.bind('<B1-Motion>', move_window)
@@ -498,7 +521,7 @@ def get_pos(event): # this is executed when the title bar is clicked to move the
         title_bar_title.bind('<ButtonRelease-1>', release_window)
     else:
         expand_button.config(text=" 🗖 ")
-        root.maximized = not root.maximized
+        main_frame.maximized = not main_frame.maximized
 
 title_bar.bind('<Button-1>', get_pos) # so you can drag the window from the title bar
 title_bar_title.bind('<Button-1>', get_pos) # so you can drag the window from the title 
@@ -517,18 +540,18 @@ resizex_widget.pack(side=RIGHT,ipadx=2,fill=Y)
 
 
 def resizex(event):
-    xwin = root.winfo_x()
-    difference = (event.x_root - xwin) - root.winfo_width()
+    xwin = main_frame.winfo_x()
+    difference = (event.x_main_frame - xwin) - main_frame.winfo_width()
     
-    if root.winfo_width() > 150 : # 150 is the minimum width for the window
+    if main_frame.winfo_width() > 150 : # 150 is the minimum width for the window
         try:
-            root.geometry(f"{ root.winfo_width() + difference }x{ root.winfo_height() }")
+            main_frame.geometry(f"{ main_frame.winfo_width() + difference }x{ main_frame.winfo_height() }")
         except:
             pass
     else:
         if difference > 0: # so the window can't be too small (150x150)
             try:
-                root.geometry(f"{ root.winfo_width() + difference }x{ root.winfo_height() }")
+                main_frame.geometry(f"{ main_frame.winfo_width() + difference }x{ main_frame.winfo_height() }")
             except:
                 pass
               
@@ -541,18 +564,18 @@ resizey_widget = Frame(window,bg=DGRAY,cursor='sb_v_double_arrow')
 resizey_widget.pack(side=BOTTOM,ipadx=2,fill=X)
 
 def resizey(event):
-    ywin = root.winfo_y()
-    difference = (event.y_root - ywin) - root.winfo_height()
+    ywin = main_frame.winfo_y()
+    difference = (event.y_main_frame - ywin) - main_frame.winfo_height()
 
-    if root.winfo_height() > 150: # 150 is the minimum height for the window
+    if main_frame.winfo_height() > 150: # 150 is the minimum height for the window
         try:
-            root.geometry(f"{ root.winfo_width()  }x{ root.winfo_height() + difference}")
+            main_frame.geometry(f"{ main_frame.winfo_width()  }x{ main_frame.winfo_height() + difference}")
         except:
             pass
     else:
         if difference > 0: # so the window can't be too small (150x150)
             try:
-                root.geometry(f"{ root.winfo_width()  }x{ root.winfo_height() + difference}")
+                main_frame.geometry(f"{ main_frame.winfo_width()  }x{ main_frame.winfo_height() + difference}")
             except:
                 pass
 
@@ -561,8 +584,8 @@ def resizey(event):
 resizey_widget.bind("<B1-Motion>",resizey)
 
 # some settings
-root.bind("<FocusIn>",deminimize) # to view the window by clicking on the window icon on the taskbar
-root.after(10, lambda: set_appwindow(root)) # to see the icon on the task bar
+main_frame.bind("<FocusIn>",deminimize) # to view the window by clicking on the window icon on the taskbar
+main_frame.after(10, lambda: set_appwindow(main_frame)) # to see the icon on the task bar
 
 
 # pack the widgets
