cdd308ad45b4faa18e1bd4d0f31c65d080099f49
Федор Глеб | abhi <59841219+snehccurry@users.noreply.github.com>
2024-04-12 14:46:45 +0530

    added toggle_switch

--- __init__.py
+++ __init__.py
@@ -220,7 +220,39 @@ def scrollable_frame(window, bg="#202020",y=True,x=False):
     
 
 
+class SwitchButton(Button):
+    def __init__(self, *args, on_click=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.heema_icons = tkextrafont.Font(file="./heema-icons.ttf", family="heema-icons")
+        self._state = "on"
+        self.configure(bd=label_bd,activebackground=self.master.cget("background"),activeforeground=self.master.cget("background"),background=self.master.cget("background"))  # Set button background to root's background
+        self.update_text()  # Update button text based on initial state
 
+        if on_click:
+            self.bind("<Button-1>", lambda event: self.execute_functions(on_click))
+
+    def state(self, new_state=None):
+        if new_state is not None:
+            self._state = new_state
+            self.update_text()  # Update button text when state changes
+        return self._state
+
+    def toggle(self):
+        if self._state == "on":
+            self._state = "off"
+        else:
+            self._state = "on"
+        self.update_text()  # Update button text based on new state
+
+    def update_text(self):
+        toggle_on_symbol = u"\ued0a"  # Symbol for the "on" state
+        toggle_off_symbol = u"\ued09"  # Symbol for the "off" state
+        toggle_text = toggle_off_symbol if self._state == "off" else toggle_on_symbol
+        self.config(text=toggle_text, fg="#565656" if self._state == "off" else "#ffffff")
+
+    def execute_functions(self, on_click):
+        self.toggle()  # Toggle button state
+        on_click()  # Execute the function passed as argument
 
 
 class create_safe_keypad:
