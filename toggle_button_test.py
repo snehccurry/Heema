c38308e2e50abe67e55990261ad7a6499b3b554d
Федор Глеб | abhi <59841219+snehccurry@users.noreply.github.com>
2024-04-15 11:20:34 +0530

    Update toggle_button_test.py

--- toggle_button_test.py
+++ toggle_button_test.py
@@ -1,44 +1,22 @@
-from Heema import *
-
-from tkextrafont import Font
-
-state = "on"
-
-toggle_on_symbol = u"\ued0a"  # Symbol for the "on" state
-toggle_off_symbol = u"\ued09"  # Symbol for the "off" state
-
-settings_page = create_window()
-heema_icons = Font(file="heema-icons.ttf", family="heema-icons")
+from Heema import create_window, Button,label_bd
+import tkextrafont
 
 
-def turn_on_or_off():
-    global state  # Access and update the global variable 'state'
-    toggle_text = toggle_off_symbol if state == "on" else toggle_on_symbol
 
-    if (state == "on"):
-        state = "off"
-        switch_button1.config(text=toggle_text, fg="#787878")
-        switch_button1.update()
+
+
+root=create_window()
+
+def update():
+    if(x.state()=="on"):
+        print("settings is on")
     else:
-        state = "on"
-        switch_button1.config(text=toggle_text, fg="#ffffff")
-        switch_button1.update()
+        print("settings is off")
 
 
-def toggle_switch_button(frame_name, text, command=do_nothing):
-    l = Button(frame_name, font=('calibri', "11"), text=text, border=label_bd, bg=label_bg, fg="white", command=command)
-    l.config(activebackground="#202020", activeforeground="white", bg="#202020", fg="#ffffff", )  # 018574
+x=SwitchButton(root,on_click=update,toggle_state="on",font=("Segoe UI",40),)
 
-    return l
+x.pack()
 
 
-switch_button1 = toggle_switch_button(
-    frame_name=settings_page, text=toggle_on_symbol if state == "on" else "off", command=turn_on_or_off
-)
-
-switch_button1.config(font=("SegoeUI", 50))
-switch_button1.pack()
-
-
-make_rounded(settings_page)
-settings_page.mainloop()
+root.mainloop()
